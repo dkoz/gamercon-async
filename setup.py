e1ac5ca45b2fc43fa5555caff863d1cf464be64c
dkoz <drunkenkoz@gmail.com>
2024-01-16 19:44:48 -0500

    Update

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='gamercon-async',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'asyncio',
-        'struct',
     ],
     python_requires='>=3.6',  # specify your Python version requirement
     description='An async rcon client for video games.',
